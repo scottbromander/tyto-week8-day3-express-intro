ddc82d0ed2406868c10c5d07ef8741fb869a49ce
Scott Bromander <scottbromander@gmail.com>
2019-11-20 10:23:32 -0600

    update for lecture

--- server.js
+++ server.js
@@ -1,10 +1,33 @@
 const express = require('express');
 const app = express();
 
-app.listen(5000, function() {
+const name = 'Scott';
+const person = {
+    firstName: 'Scott',
+    lastName: 'Bromander',
+    company: 'Prime Academy'
+}
+const array = [1,2,3];
+const rawNumber = 5;
+
+app.listen(5000, () => {
     console.log('Its on');
 });
 
-app.get('/', function(req,res) {
-    res.send('its on!');
+//
+app.get('/', (req,res) => {
+    res.send({ value: rawNumber });
 });
+
+// http://localhost:5000/person
+app.get('/person', (req,res) => {
+    res.send(person);
+})
+
+app.get('/kittycat', (req,res) => {
+    res.send(array);
+});
+
+app.get('/something', (req,res) => {
+    res.send({value :array[0]})
+})
